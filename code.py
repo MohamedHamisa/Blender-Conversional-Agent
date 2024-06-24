7731d1d5aee998a2df78f757894890e59df4d0b5
Mohamed Hamisa <108889205+MohamedHamisa@users.noreply.github.com>
2024-06-24 18:56:14 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,9 +1,8 @@
-# Install Dependencies
+import micropip
 
-!pip install torch==1.9.1+cu111 torchvision==0.10.1+cu111 torchaudio===0.9.1 -f https://download.pytorch.org/whl/torch_stable.html
-!pip install transformers
+# Install transformers using micropip
+await micropip.install('transformers')
 
-# Import Model
 # Import the model class and the tokenizer
 from transformers import BlenderbotTokenizer, BlenderbotForConditionalGeneration
 
@@ -12,19 +11,16 @@ tokenizer = BlenderbotTokenizer.from_pretrained("facebook/blenderbot-400M-distil
 model = BlenderbotForConditionalGeneration.from_pretrained("facebook/blenderbot-400M-distill")
 
 # Speak to it!
-
 utterance = "I want to order a Pizza"
 
 # Tokenize the utterance
 inputs = tokenizer(utterance, return_tensors="pt") # pytorch tensor format
-inputs
 
 # Passing through the utterances to the Blenderbot model
 res = model.generate(**inputs)
-res
 
 # Decoding the model output
-tokenizer.decode(res[0])
+print(tokenizer.decode(res[0]))
 
 # Decoding the inputs
-tokenizer.decode(inputs['input_ids'][0])
+print(tokenizer.decode(inputs['input_ids'][0]))
